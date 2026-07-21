36d048d884935ec4668d3fae1e4e6313243def25
Vz-Utsav <utsav.kcipl@gmail.com>
2026-07-21 16:21:58 +0530

    commit

--- nextjs-keyconcepts/src/lib/api.js
+++ nextjs-keyconcepts/src/lib/api.js
@@ -1,6 +1,6 @@
 const API_BASE_URL =
-  // process.env.NEXT_PUBLIC_API_BASE_URL ||
-  // process.env.VITE_API_BASE_URL ||
+  process.env.NEXT_PUBLIC_API_BASE_URL ||
+  process.env.VITE_API_BASE_URL ||
   "http://localhost:5000/api";
 
 async function request(path, options = {}) {
